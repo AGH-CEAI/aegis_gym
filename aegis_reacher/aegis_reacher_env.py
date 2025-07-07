4eeb7349bdf41f67fe8108044a27d53db09017b6
Jakub <sivral@proton.me>
2025-07-07 14:47:06 +0200

    Change home position

--- aegis_reacher/aegis_reacher_env.py
+++ aegis_reacher/aegis_reacher_env.py
@@ -50,7 +50,7 @@ class ROSInterface:
         self.joint_names = self.robot_director.joint_names
         self.dof_home = {
             name: value
-            for name, value in zip(self.joint_names, [0.0, -1.57, 1.57, 0.0, 0.0, 0.0])
+            for name, value in zip(self.joint_names, [0.0, -2.09, 2.09, -1.57, -1.57, 1.57])
         }
         self._initialized = True
 
