ab64a9e48b1391e61dd251c3d82840179adeadae
Maciej Aleksandrowicz <mac.aleksandrowicz@gmail.com>
2025-07-07 14:42:02 +0200

    Update aegis_gym/__init__.py

--- aegis_gym/__init__.py
+++ aegis_gym/__init__.py
@@ -9,18 +9,18 @@ from gymnasium.envs.registration import register
 
 ENV_IDS = []
 
-for task in ["Reacher"]:
-    control_suffix = "Joints"
-    reward_suffix = "Dense"
-    env_id = f"Aegis{task}{control_suffix}{reward_suffix}-v1"
+task = "Reacher"
+control_suffix = "Joints"
+reward_suffix = "Dense"
+env_id = f"Aegis{task}{control_suffix}{reward_suffix}-v1"
 
-    register(
-        id=env_id,
-        entry_point=f"aegis_gym.envs:Aegis{task}Env",
-        kwargs={
-            "reward_type": reward_suffix.lower(),
-            "control_type": control_suffix.lower(),
-        },
-        max_episode_steps=50,
-    )
-    ENV_IDS.append(env_id)
+register(
+    id=env_id,
+    entry_point=f"aegis_gym.envs:Aegis{task}Env",
+    kwargs={
+        "reward_type": reward_suffix.lower(),
+        "control_type": control_suffix.lower(),
+    },
+    max_episode_steps=50,
+)
+ENV_IDS.append(env_id)
