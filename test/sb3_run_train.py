756cdc34b1b5759135d09087849aafaa84f3420c
macmacal <mac.aleksandrowicz@gmail.com>
2025-09-15 19:14:01 +0200

    ADD: sample sb3 test train

---
+++ test/sb3_run_train.py
@@ -0,0 +1,38 @@
+import time
+
+import gymnasium as gym
+
+import aegis_gym
+from aegis_gym.sim.utils import TorchToNumpyWrapper
+
+try:
+    from stable_baselines3 import PPO
+except ImportError:
+    print(
+        ">>> To run this SB3 integration test, you need SB3: pip install stable-baselines3"
+    )
+    PPO = None
+
+
+def main():
+    # env_name = sim_genesis.ENV_IDS[0]
+    env_name = aegis_gym.ENV_IDS[0]
+
+    print(f"Training on environment: {env_name}")
+
+    env = gym.make(env_name)
+    env = TorchToNumpyWrapper(env)
+
+    # Sleep 5 seconds and print countdown
+    for i in range(5, 0, -1):
+        print(f"Starting in {i} seconds...", end="\r")
+        time.sleep(1)
+    print("Starting now!            ")
+
+    model = PPO("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps=10_000)
+
+
+if __name__ == "__main__":
+    if PPO:
+        main()
