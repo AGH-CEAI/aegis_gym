30a78085cf800128ff955f09a3d7c10912c91b63
Jakub <sivral@proton.me>
2025-12-17 16:32:27 +0100

    Add optional cell visualization for URDF generation

--- aegis_gym/sim/utils.py
+++ aegis_gym/sim/utils.py
@@ -34,13 +34,18 @@ class TorchToNumpyWrapper(gym.ObservationWrapper, gym.ActionWrapper):
         return action
 
 
-def generate_aegis_urdf() -> Path:
+def generate_aegis_urdf(show_cell: bool = True) -> Path:
     pkg_share = Path(get_package_share_directory("aegis_description"))
     xacro_path = pkg_share / "urdf" / "aegis.urdf.xacro"
     _, urdf_path = tempfile.mkstemp(suffix=".urdf", prefix="aegis_urdf_", dir="/tmp")
 
+    if show_cell:
+        xacro_args = ["disable_cell_collision:=true", "disable_cell:=false"]
+    else:
+        xacro_args = ["disable_cell:=true"]
+
     urdf_with_uris = subprocess.run(
-        ["xacro", str(xacro_path)],
+        ["xacro", str(xacro_path)] + xacro_args,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         check=True,
