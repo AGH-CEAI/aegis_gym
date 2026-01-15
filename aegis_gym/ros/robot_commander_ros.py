1abbecf8883014eb89f704a93830580bd869627f
macmacal <mac.aleksandrowicz@gmail.com>
2026-01-15 14:59:57 +0100

    removed aegis_director; added TensorDict

--- aegis_gym/ros/robot_commander_ros.py
+++ aegis_gym/ros/robot_commander_ros.py
@@ -1,6 +1,8 @@
 from typing import Optional
+
 import torch as th
 
+from tensordict import TensorDict
 
 try:
     from aegis_grpc_client import AegisRobotClient    
@@ -26,47 +28,47 @@ class RobotCommanderROS(RobotCommanderInterface):
     def __init__(
         self, robot_client: AegisRobotClient, device: str
     ) -> None:
-        if hasattr(self, "_initialized") and self._initialized:
-            return
         super().__init__(device=device)
-        self.robot_director = robot_director
-        self.robot_client = robot_client
+        self._robot_client = robot_client
+        
+        # Dynamicly obtain joints names
+        self.joint_names = list(self._robot_client.get_joint_names())[1:]
+        
+        # Prepare initial observation
+        self._state: Optional[TensorDict] = None
+        self.read_state()
+        
 
-        # TODO rewrite all getters to use robot_client
-        joint_state = self.robot_director._get_joint_states()
-        self.joint_names = list(joint_state.name)[1:]
-        self._initialized = True
+    def read_state(self) -> None:
+        state = self._robot_client.get_all()
+        self._state = TensorDict({
+            "pose": th.from_numpy(state["pose"]),
+            "wrench": th.from_numpy(state["wrench"]),
+            "joints_pos": th.from_numpy(state["joints_pos"]),
+            "joints_vel": th.from_numpy(state["joints_vel"]),
+            "joints_eff": th.from_numpy(state["joints_eff"]),
+        })
+        
+    def get_state_tensordict(self) -> TensorDict:
+        return self._state
 
-    def get_joint_positions(self) -> th.Tensor:
-        jp = self.robot_director.get_joint_positions()
-        return th.tensor(
-            [jp[name] for name in self.joint_names],
-            dtype=th.float32,
-            device=self.device,
-        )
+    def get_joints_positions(self) -> th.Tensor:
+        return self._state["joints_pos"].to(device=self.device, dtype=th.float32)
 
-    def get_joint_velocities(self) -> th.Tensor:
-        jv = self.robot_director.get_joint_velocities()
-        return th.tensor(
-            [jv[name] for name in self.joint_names],
-            dtype=th.float32,
-            device=self.device,
-        )
+    def get_joints_velocities(self) -> th.Tensor:
+        return self._state["joints_vel"].to(device=self.device, dtype=th.float32)
+    
+    def get_joints_efforts(self) -> th.Tensor:
+        return self._state["joints_eff"].to(device=self.device, dtype=th.float32)
 
     def get_tcp_position(self) -> th.Tensor:
-        return self.get_tcp_pose()[:3]
+        return self._state["pose"].to(device=self.device, dtype=th.float32)[:3]
 
     def get_tcp_orientation(self) -> th.Tensor:
-        return self.get_tcp_pose()[3:]
+        return self._state["pose"].to(device=self.device, dtype=th.float32)[3:]
 
     def get_tcp_pose(self) -> th.Tensor:
-        tcp = self.robot_director.get_tcp_pose()
-        return th.cat(
-            [
-                th.tensor(tcp["position"], dtype=th.float32, device=self.device),
-                th.tensor(tcp["orientation"], dtype=th.float32, device=self.device),
-            ],
-        )
+        return self._state["pose"].to(device=self.device, dtype=th.float32)
 
     def get_base_position(self) -> th.Tensor:
         return th.zeros(3, dtype=th.float32, device=self.device)
@@ -74,39 +76,46 @@ class RobotCommanderROS(RobotCommanderInterface):
     def control_dofs_position(
         self, target_pos: th.Tensor, max_vel: float = 0.3, max_accel: float = 0.3
     ) -> None:
-        if self.robot_director.servo_enabled:
-            self.robot_director.servo_disable()
+        # TODO implement Moveit2 control via gRPC
+        pass
+        # if self.robot_director.servo_enabled:
+        #     self.robot_director.servo_disable()
 
-        target_pos_np = target_pos.detach().cpu().numpy()
-        target_pos_dict = {
-            name: float(pos) for name, pos in zip(self.joint_names, target_pos_np)
-        }
-        self.robot_director.joint_move(
-            joint_positions=target_pos_dict, max_vel=max_vel, max_accel=max_accel
-        )
+        # target_pos_np = target_pos.detach().cpu().numpy()
+        # target_pos_dict = {
+        #     name: float(pos) for name, pos in zip(self.joint_names, target_pos_np)
+        # }
+        # self.robot_director.joint_move(
+        #     joint_positions=target_pos_dict, max_vel=max_vel, max_accel=max_accel
+        # )
 
     def control_dofs_position_servo(
         self, target_pos: th.Tensor, max_vel: float = 0.3, max_accel: float = 0.3
     ) -> None:
+        # TODO enable servo control
+        pass
         # TODO(issue#35) - Unify servoing into position or velocities commands
-        self.control_dofs_velocity_servo(target_vel=target_pos)
+        # self.control_dofs_velocity_servo(target_vel=target_pos)
 
     def control_dofs_velocity_servo(
         self,
         target_vel: th.Tensor | None = None,
     ) -> None:
-        if not self.robot_director.servo_enabled:
-            self.robot_director.servo_enable()
+        # TODO enable servo control
+        pass
+    
+        # if not self.robot_director.servo_enabled:
+        #     self.robot_director.servo_enable()
 
-        if target_vel is None:
-            target_vel_tuple = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-        else:
-            target_vel_tuple = tuple(target_vel.detach().cpu().numpy())
+        # if target_vel is None:
+        #     target_vel_tuple = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        # else:
+        #     target_vel_tuple = tuple(target_vel.detach().cpu().numpy())
 
-        self.robot_director.servo_jog(
-            joint_names=tuple(self.joint_names),
-            velocities=target_vel_tuple,
-        )
+        # self.robot_director.servo_jog(
+        #     joint_names=tuple(self.joint_names),
+        #     velocities=target_vel_tuple,
+        # )
 
     def control_tcp_position(
         self,
@@ -115,28 +124,30 @@ class RobotCommanderROS(RobotCommanderInterface):
         max_vel: float = 0.3,
         max_accel: float = 0.3,
     ) -> None:
-        if self.robot_director.servo_enabled:
-            self.robot_director.servo_disable()
+        # TODO implement Moveit2 control via gRPC
+        pass
+        # if self.robot_director.servo_enabled:
+        #     self.robot_director.servo_disable()
 
-        if target_ori is None:
-            target_ori = th.tensor(
-                [1.0, 0.0, 0.0, 0.0],
-                dtype=th.float32,
-                device=self.device,
-            )
+        # if target_ori is None:
+        #     target_ori = th.tensor(
+        #         [1.0, 0.0, 0.0, 0.0],
+        #         dtype=th.float32,
+        #         device=self.device,
+        #     )
 
-        target_pos_np = target_pos.detach().cpu().numpy()
-        target_ori_np = target_ori.detach().cpu().numpy()
+        # target_pos_np = target_pos.detach().cpu().numpy()
+        # target_ori_np = target_ori.detach().cpu().numpy()
 
-        target_pos_tuple = tuple(float(v) for v in target_pos_np)
-        target_ori_tuple = tuple(float(v) for v in target_ori_np)
+        # target_pos_tuple = tuple(float(v) for v in target_pos_np)
+        # target_ori_tuple = tuple(float(v) for v in target_ori_np)
 
-        self.robot_director.pose_move(
-            position=target_pos_tuple,
-            quat_xyzw=target_ori_tuple,
-            max_vel=max_vel,
-            max_accel=max_accel,
-        )
+        # self.robot_director.pose_move(
+        #     position=target_pos_tuple,
+        #     quat_xyzw=target_ori_tuple,
+        #     max_vel=max_vel,
+        #     max_accel=max_accel,
+        # )
 
     # TODO(issue#33): Change servo API to Euler angles
     def control_tcp_position_servo(
@@ -146,8 +157,11 @@ class RobotCommanderROS(RobotCommanderInterface):
         max_vel: float = 0.3,
         max_accel: float = 0.3,
     ) -> None:
+        # TODO enable servo control
+        pass
+        
         # TODO(issue#35) - Unify servoing into position or velocities commands
-        self.control_tcp_velocity_servo(target_pos, target_ori)
+        # self.control_tcp_velocity_servo(target_pos, target_ori)
 
     # TODO(issue#33): Change servo API to Euler angles
     def control_tcp_velocity_servo(
@@ -155,29 +169,33 @@ class RobotCommanderROS(RobotCommanderInterface):
         target_pos: th.Tensor,
         target_ori: th.Tensor | None = None,
     ) -> None:
-        if not self.robot_director.servo_enabled:
-            self.robot_director.servo_enable()
+        # TODO enable servo control
+        pass
+        # if not self.robot_director.servo_enabled:
+        #     self.robot_director.servo_enable()
 
-        if target_ori is None:
-            target_ori_tuple = tuple([0.0, 0.0, 0.0])
-        else:
-            target_ori_np = target_ori.detach().cpu().numpy()
-            target_ori_tuple = tuple(quaternion_to_euler(q_xyzw=target_ori_np))
+        # if target_ori is None:
+        #     target_ori_tuple = tuple([0.0, 0.0, 0.0])
+        # else:
+        #     target_ori_np = target_ori.detach().cpu().numpy()
+        #     target_ori_tuple = tuple(quaternion_to_euler(q_xyzw=target_ori_np))
 
-        target_pos_np = target_pos.detach().cpu().numpy()
-        target_pos_tuple = tuple(float(v) for v in target_pos_np)
+        # target_pos_np = target_pos.detach().cpu().numpy()
+        # target_pos_tuple = tuple(float(v) for v in target_pos_np)
 
-        self.robot_director.servo_move(
-            linear=target_pos_tuple,
-            angular=target_ori_tuple,
-        )
+        # self.robot_director.servo_move(
+        #     linear=target_pos_tuple,
+        #     angular=target_ori_tuple,
+        # )
 
     def move_to_home(self) -> None:
-        if self.robot_director.servo_enabled:
-            self.robot_director.servo_disable()
-        # TODO(issue#34) There are edge positions where the Moveit2 planner can't plan the trajectory to home position. This issue breaks the training.
-        self.robot_director.joint_move(
-            joint_positions=self.dof_home_dict,
-            max_vel=0.5,
-            max_accel=0.5,
-        )
+        # TODO implement Moveit2 control via gRPC
+        pass
+        # if self.robot_director.servo_enabled:
+        #     self.robot_director.servo_disable()
+        # # TODO(issue#34) There are edge positions where the Moveit2 planner can't plan the trajectory to home position. This issue breaks the training.
+        # self.robot_director.joint_move(
+        #     joint_positions=self.dof_home_dict,
+        #     max_vel=0.5,
+        #     max_accel=0.5,
+        # )
--- aegis_gym/ros/scene_director_ros.py
+++ aegis_gym/ros/scene_director_ros.py
@@ -1,19 +1,6 @@
 import asyncio
 from typing import Optional
 
-import rclpy
-
-
-try:
-    from aegis_director.robot_director import RobotDirector
-except ImportError:
-    print(
-        "Failed to import aegis_director. Double check if you have sourced the AGH-CEAI/aegis_ros project."
-    )
-    RobotDirector = None
-if RobotDirector is None:
-    raise ImportError
-
 try:
     from aegis_grpc_client import AegisRobotClient
 except ImportError:
@@ -21,7 +8,7 @@ except ImportError:
         "Failed to import aegis_grpc_client. "
         "Double check if you have installed the `aegis_grpc_client` and `proto_aegis_grpc` packages."
     )
-    raise ImportError
+    raise
 
 from ..scene import SceneDirectorInterface, EntityType, SceneEntity
 from .robot_commander_ros import RobotCommanderROS
@@ -39,26 +26,18 @@ class SceneDirectorROS(SceneDirectorInterface):
     def __del__(self) -> None:
         if self.robot_client.is_connected:
             asyncio.run(self.robot_client.disconnect())
-        self.shutdown()
 
     def __init__(self, device: str = "cuda", enable_scene_camera: bool = False) -> None:
         if hasattr(self, "_initialized") and self._initialized:
             return
         super().__init__(device)
-        rclpy.init()
-        self.robot_director = RobotDirector(synchronous=True)
-        self._scene_node = rclpy.create_node("scene_director")
 
         self.robot_client = AegisRobotClient(server_address="127.0.0.1:50051")
         asyncio.run(self.robot_client.connect())
-
         self._initialized = True
 
-    def shutdown(self) -> None:
-        rclpy.shutdown()
-
     def get_robot_commander(self) -> RobotCommanderROS:
-        return RobotCommanderROS(self.robot_director, self.robot_client, self.device)
+        return RobotCommanderROS(self.robot_client, self.device)
 
     def add_entity(self, entity: EntityType) -> SceneEntity:
         return EntityTypeROS[entity](self._scene_node)
