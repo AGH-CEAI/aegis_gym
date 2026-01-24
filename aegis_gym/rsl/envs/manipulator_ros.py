5a9f5e183b84dceb7b867d91d193f870669cc552
Maciej Aleksandrowicz <mac.aleksandrowicz@gmail.com>
2026-01-24 21:08:52 +0100

    changed asyncio.run to coroutines

--- aegis_gym/rsl/envs/manipulator_ros.py
+++ aegis_gym/rsl/envs/manipulator_ros.py
@@ -1,5 +1,7 @@
 import asyncio
+import threading
 from typing import Optional
+from concurrent.futures import Future
 
 import torch as th
 from tensordict import TensorDict
@@ -23,8 +25,11 @@ class ManipulatorROS:
         return cls._instance
 
     def __del__(self) -> None:
-        if self.robot_client.is_connected:
-            asyncio.run(self.robot_client.disconnect())
+        if hasattr(self, "_robot_client") and self._robot_client.is_connected:
+            self._run_coro(self._robot_client.disconnect())
+        if hasattr(self, "_loop") and self._loop.is_running():
+            self._loop.call_soon_threadsafe(self._loop.stop)
+            self._loop_thread.join(timeout=2.0)
 
     def __init__(
         self,
@@ -35,18 +40,15 @@ class ManipulatorROS:
         if hasattr(self, "_initialized") and self._initialized:
             return
 
-        self.num_envs = num_envs
-        if self.num_envs > 1:
-            raise ValueError(
-                "The `num_envs` is greater than 1 for controlling just 1 robot station!!!"
-            )
+        if num_envs > 1:
+            raise ValueError("num_envs > 1 not supported for single robot station")
 
         self._num_envs = num_envs
         self._args = args
-        self._device = device
+        self.device = device
 
-        # TODO get from cfg
-        self.ctrl_dt = 1 / 250.0  # 1/servo_f
+        # TODO get from cfg / dynamically from rsl_rl
+        self.ctrl_dt = 1 / 10.0  # 1 / poliicy_f
 
         self.dof_home_dict = {
             "shoulder_pan_joint": 0.0,
@@ -55,19 +57,37 @@ class ManipulatorROS:
             "wrist_1_joint": -1.57,
             "wrist_2_joint": -1.57,
             "wrist_3_joint": 0.0,
-            # "robotiq_hande_left_finger_joint": 0.025,
-            # "robotiq_hande_right_finger_joint": 0.025,
         }
 
-        self.robot_client = AegisRobotClient(server_address="127.0.0.1:50051")
-        asyncio.run(self.robot_client.connect())
+        self._loop = asyncio.new_event_loop()
+        self._loop_thread = threading.Thread(target=self._loop.run_forever, daemon=True)
+        self._loop_thread.start()
+
+        self._robot_client = AegisRobotClient(server_address="127.0.0.1:50051")
+        self._run_coro(self._robot_client.connect())
+
+        self._run_coro(self._robot_client.gripper_open())
+        self._gripper_last_action = True
 
         # Prepare initial observation
         self._state: Optional[TensorDict] = None
         self.read_state()
 
+    def _run_loop(self) -> None:
+        """Run the event loop forever in a background thread."""
+        asyncio.set_event_loop(self._loop)
+        self._loop.run_forever()
+
+    def _run_coro(self, coro) -> any:
+        """
+        Schedule a coroutine on the persistent loop and block until done.
+        Safe to call from the main (sync) thread.
+        """
+        future: Future = asyncio.run_coroutine_threadsafe(coro, self._loop)
+        return future.result()  # blocks until complete
+
     def read_state(self) -> None:
-        state = asyncio.run(self._robot_client.get_all())
+        state = self._run_coro(self._robot_client.get_all())
         self._state = TensorDict(
             {
                 "pose": th.from_numpy(state["pose"]),
@@ -114,7 +134,7 @@ class ManipulatorROS:
         self._move_to_home()
 
     def _move_to_home(self) -> None:
-        asyncio.run(
+        self._run_coro(
             self._robot_client.goto_joints(
                 names=tuple(self.dof_home_dict.keys()),
                 positions=tuple(self.dof_home_dict.values()),
@@ -124,11 +144,14 @@ class ManipulatorROS:
     def apply_action(
         self, action: th.Tensor, open_gripper: Optional[bool] = None
     ) -> None:
-        # Change position commands into velocities for the MoveIt2 Servo
-        delta_velocity = action[:, :3] / self.ctrl_dt
-        delta_angular = action[:, 3:6] / self.ctrl_dt
+        # Control only one real robot
+        action = action.squeeze(dim=0)
 
-        asyncio.run(
+        # Change position commands into velocities for the MoveIt2 Servo
+        delta_velocity = action[:3] / self.ctrl_dt
+        delta_angular = action[3:6] / self.ctrl_dt
+
+        self._run_coro(
             self._robot_client.servo_tcp(
                 linear=delta_velocity,
                 angular=delta_angular,
@@ -138,10 +161,11 @@ class ManipulatorROS:
         if open_gripper is None:
             return
 
-        if open_gripper:
-            asyncio.run(self._robot_client.gripper_open())
-        else:
-            asyncio.run(self._robot_client.gripper_close())
+        if open_gripper and not self._gripper_last_action:
+            self._run_coro(self._robot_client.gripper_open())
+        if not open_gripper and self._gripper_last_action:
+            self._run_coro(self._robot_client.gripper_close())
+        self._gripper_last_action = open_gripper
 
     def go_to_goal(
         self, goal_pose: th.Tensor, open_gripper: Optional[bool] = None
@@ -149,7 +173,7 @@ class ManipulatorROS:
         target_pos_np = goal_pose[:, :3].detach().cpu().numpy()
         target_ori_np = goal_pose[:, 3:7].detach().cpu().numpy()
 
-        asyncio.run(
+        self._run_coro(
             self._robot_client.goto_pose(
                 position=target_pos_np,
                 orientation=target_ori_np,
@@ -159,16 +183,17 @@ class ManipulatorROS:
         if open_gripper is None:
             return
 
-        if open_gripper:
-            asyncio.run(self._robot_client.gripper_open())
-        else:
-            asyncio.run(self._robot_client.gripper_close())
+        if open_gripper and not self._gripper_last_action:
+            self._run_coro(self._robot_client.gripper_open())
+        if not open_gripper and self._gripper_last_action:
+            self._run_coro(self._robot_client.gripper_close())
+        self._gripper_last_action = open_gripper
 
     @property
     def base_pos(self) -> th.Tensor:
-        return self.get_base_position()
+        return self.get_base_position().unsqueeze(dim=0)
 
     @property
     def ee_pose(self) -> th.Tensor:
         # TODO validate if its the actual gripper's EE
-        return self.get_tcp_pose()
+        return self.get_tcp_pose().unsqueeze(dim=0)
