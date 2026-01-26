8a4c49f72c1d63c215228da4a6e1466d3cc9843e
macmacal <mac.aleksandrowicz@gmail.com>
2026-01-26 18:04:56 +0100

    grasp_train: automatic device selection

--- aegis_gym/rsl/grasp_train.py
+++ aegis_gym/rsl/grasp_train.py
@@ -43,6 +43,7 @@ def main():
     # BC only needs a small number of envs
     env_cfg["num_envs"] = args.num_envs if args.stage == "rl" else 10
 
+    device = th.device("cuda" if th.cuda.is_available() else "cpu")
     env = None
     if args.control == "sim":
         from envs.grasp_env import GraspEnv
@@ -65,7 +66,7 @@ def main():
             reward_cfg=reward_scales,
             robot_cfg=robot_cfg,
             show_viewer=args.vis,
-            device=th.device("cuda" if th.cuda.is_available() else "cpu"),
+            device=device,
         )
 
     if env is None:
@@ -76,10 +77,10 @@ def main():
     if args.stage == "bc":
         teacher_policy = load_teacher_policy(env, rl_train_cfg, args.exp_name)
         bc_train_cfg["teacher_policy"] = teacher_policy
-        runner = BehaviorCloning(env, bc_train_cfg, teacher_policy, device=gs.device)
+        runner = BehaviorCloning(env, bc_train_cfg, teacher_policy, device=device)
         runner.learn(num_learning_iterations=args.max_iterations, log_dir=log_dir)
     else:
-        runner = OnPolicyRunner(env, rl_train_cfg, log_dir, device=gs.device)
+        runner = OnPolicyRunner(env, rl_train_cfg, log_dir, device=device)
         runner.learn(
             num_learning_iterations=args.max_iterations, init_at_random_ep_len=True
         )
