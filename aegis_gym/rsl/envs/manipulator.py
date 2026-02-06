26a90fb79ced4261c0b5ba2aa4c69562b922bb1a
macmacal <mac.aleksandrowicz@gmail.com>
2026-02-06 17:04:39 +0100

    reverted calibration to defaults with coeffs

--- aegis_gym/rsl/envs/manipulator.py
+++ aegis_gym/rsl/envs/manipulator.py
@@ -101,14 +101,14 @@ class Manipulator:
     def set_pd_gains(self):
         # set control gains
         self._robot_entity.set_dofs_kp(
-            th.tensor([6200, 6200, 6800, 3500, 2500, 3500, 100, 100]),
+            th.tensor([4500, 4500, 3500, 3500, 2000, 2000, 100, 100]) * 10.0,
         )
         self._robot_entity.set_dofs_kv(
-            th.tensor([1050, 1125, 280, 180, 320, 590, 10, 10]),
+            th.tensor([350, 350, 250, 250, 100, 100, 10, 10]) * 1.4,
         )
         self._robot_entity.set_dofs_force_range(
-            th.tensor([-15, -27, -49, -10, -7, -7, -100, -100]),
-            th.tensor([15, 27, 49, 10, 7, 7, 100, 100]),
+            th.tensor([-87, -87, -87, -87, -12, -12, -100, -100]) * 10.0,
+            th.tensor([87, 87, 87, 87, 12, 12, 100, 100]) * 10.0,
         )
         # self._robot_entity.set_dofs_armature(
         #     th.tensor([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
@@ -156,6 +156,7 @@ class Manipulator:
         """
         Apply the action to the robot.
         """
+        print(f"> ACTION LOOKS LIKE: {action}")
         q_pos = self._robot_entity.get_qpos()
         if self._ik_method == "gs_ik":
             q_pos = self._gs_ik(action)
