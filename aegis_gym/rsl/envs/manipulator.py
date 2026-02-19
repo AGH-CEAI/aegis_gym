a586674b79f39f42cbb1f1d6ef471687538f32fb
macmacal <mac.aleksandrowicz@gmail.com>
2026-02-19 12:32:53 +0100

    grasp: Enabled gravity compensation

--- aegis_gym/rsl/envs/manipulator.py
+++ aegis_gym/rsl/envs/manipulator.py
@@ -40,8 +40,8 @@ class Manipulator:
         print(f"[GraspEnv::Manipulator] URDF path: {self._urdf_path}")
 
         # == Genesis configurations ==
-        material: gs.materials.Rigid = gs.materials.Rigid()
-        morph: gs.morphs.URDF = gs.morphs.URDF(
+        material = gs.materials.Rigid(gravity_compensation=1.0)
+        morph = gs.morphs.URDF(
             file=self._urdf_path,
             fixed=True,
             pos=(0.0, 0.0, 0.0),
