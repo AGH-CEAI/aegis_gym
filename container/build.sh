dedc8b70f62491bf7659511f110306146ba305db
macmacal <mac.aleksandrowicz@gmail.com>
2026-03-03 15:12:10 +0100

    fixes

--- container/build.sh
+++ container/build.sh
@@ -2,7 +2,7 @@
 # Usage: setup the following values and run the script
 
 VERSION=${VERSION:-v1.0.0}
-REGISTRY=${REGISTRY:-geonosis:5000}
+REGISTRY=${REGISTRY:-geonosis.local:5000}
 PUSH=${PUSH:-false}
 
 # TODO change default tags before merge
@@ -16,26 +16,26 @@ if [[ ! "$REGISTRY" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}(:[0-9]+)?$|^[a-zA-Z0-9.-]+(:
   exit 1
 fi
 
-echo "> Building ${REGISTRY}/ceai/aegis_gym:${VERSION} variants"
+echo "> Building ${REGISTRY}/agh-ceai/aegis_gym:${VERSION} variants"
 
 podman build \
   --target simulation \
-  -t "${REGISTRY}/ceai/aegis_gym:${VERSION}-simulator" \
+  -t "${REGISTRY}/agh-ceai/aegis_gym:${VERSION}-simulator" \
   --build-arg AEIGS_GYM_TAG=${AEIGS_GYM_TAG} \
   .
 
 podman build \
   --target hardware_control \
-  -t "${REGISTRY}/ceai/aegis_gym:${VERSION}-hardware" \
+  -t "${REGISTRY}/agh-ceai/aegis_gym:${VERSION}-hardware" \
   --build-arg AEGIS_ROS_TAG=${AEGIS_ROS_TAG} \
-  --build-arg AEIGS_GYM_TAG=${AEIGS_GYM_TA} \
+  --build-arg AEIGS_GYM_TAG=${AEIGS_GYM_TAG} \
   .
 
-echo "> Built: ${REGISTRY}/ceai/aegis_gym:${VERSION}-simulator and :${VERSION}-hardware"
+echo "> Built: ${REGISTRY}/agh-ceai/aegis_gym:${VERSION}-simulator and :${VERSION}-hardware"
 
 # Optional: Push if flag set
-if [[ "${}" == "true" ]]; then
-  podman push "${REGISTRY}/ceai/aegis_gym:${VERSION}-simulator"
-  podman push "${REGISTRY}/ceai/aegis_gym:${VERSION}-hardware"
+if [[ "${PUSH}" == "true" ]]; then
+  podman push "${REGISTRY}/agh-ceai/aegis_gym:${VERSION}-simulator"
+  podman push "${REGISTRY}/agh-ceai/aegis_gym:${VERSION}-hardware"
   echo "> Pushed to $REGISTRY"
 fi
