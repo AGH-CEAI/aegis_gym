06b5620ffc2e1ead6d48d4dbbfcde302c6267d29
Jakub <sivral@proton.me>
2026-03-05 14:17:31 +0100

    Add flag to disable visual observations

--- aegis_gym/rsl/grasp_train.py
+++ aegis_gym/rsl/grasp_train.py
@@ -32,6 +32,7 @@ def main():
     parser.add_argument("--calibration-move-cart", type=str_to_list, default=None)
     parser.add_argument("--calibration-steps", type=int, default=500)
     parser.add_argument("--visualize-camera", action="store_true", default=False)
+    parser.add_argument("--disable-vision", action="store_true", default=False)
     args = parser.parse_args()
 
     # Set PyTorch default dtype to float32 for better performance
@@ -88,6 +89,7 @@ def main():
         env = GraspEnvROS(
             env_cfg=env_cfg,
             robot_cfg=robot_cfg,
+            disable_vision=args.disable_vision,
             device=device,
         )
 
