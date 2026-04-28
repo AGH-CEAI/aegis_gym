755a93043a93bdd5c981d911e7f8600036c34bbb
macmacal <mac.aleksandrowicz@gmail.com>
2026-04-28 18:55:23 +0200

    added more info for enqueue script

--- utils/clearml_enqueue_tasks.py
+++ utils/clearml_enqueue_tasks.py
@@ -12,11 +12,83 @@ Enqueue a batch of tasks to a given ClearML queue.
 
 import argparse
 import json
+import logging
+import sys
+import time
 from typing import Iterable, Any
+
 from clearml import Task
 from clearml.automation import ClearmlJob
 
+from helpers.logging import timed, setup_logging
+
 TASKS_NUM_SAFEGUARD_LIMIT = 100
+
+
+@timed
+def main():
+    """
+    Command-line interface for enqueuing tasks to a ClearML queue.
+
+    Usage: python enqueue_tasks.py <base_task_id> <tasks_num> <queue_name> [--tags TAG1 TAG2] [--parameters JSON_STRING]
+    """
+    log = logging.getLogger(f"{__name__}")
+
+    args = build_parser().parse_args()
+    parameters = json.loads(args.parameters) if args.parameters else {}
+
+    if not args.tasks_num < TASKS_NUM_SAFEGUARD_LIMIT:
+        log.warning(
+            f"You are trying to enqueue {args.tasks_num} tasks. "
+            f"The safeguard limit is {TASKS_NUM_SAFEGUARD_LIMIT} tasks. "
+            "If you REALLY want to eqnue that much, please add the `--allow-large-batch` flag."
+        )
+        time.sleep(1)
+        log.info("Exiting.")
+        return
+
+    log.info(
+        "Proceeding to enquee:\n"
+        f"\tBase task: \t{args.base_task_id}"
+        f"\tNum of tasks: \t{args.tasks_num}"
+        f"\tQueue name: \t{args.queue_name}"
+        f"\tTags: \t{args.tags}"
+        f"\tExtra parameters: \t{parameters}"
+    )
+    sleep_countdown(5)
+
+    enqueue_tasks(
+        base_task_id=args.base_task_id,
+        tasks_num=args.tasks_num,
+        queue_name=args.queue_name,
+        tags=args.tags,
+        parameters=parameters,
+    )
+    log.info(f"{args.tasks_num} tasks enqeued to {args.queue_name} queue.")
+
+
+def build_parser() -> argparse.ArgumentParser:
+    p = argparse.ArgumentParser(
+        description="Enqueue a batch of tasks to a given ClearML queue."
+    )
+    p.add_argument(
+        "--base-task-id", help="The ID of the base task to clone for each job."
+    )
+    p.add_argument("--tasks-num", type=int, help="The number of tasks to enqueue.")
+    p.add_argument(
+        "--queue-name", help="The name of the ClearML queue to enqueue the tasks to."
+    )
+    p.add_argument("--tags", nargs="*", help="Optional tags to add to each task.")
+    p.add_argument(
+        "--parameters", help="Optional parameter overrides as a JSON string."
+    )
+    p.add_argument(
+        "--allow-large-batch",
+        action="store_true",
+        default=False,
+        help="Disable safeguard mechanism for sending a large batch of runs.",
+    )
+    return p
 
 
 def enqueue_tasks(
@@ -53,54 +125,23 @@ def enqueue_tasks(
         job.launch(queue_name=queue_name)
 
 
-def main():
-    """
-    Command-line interface for enqueuing tasks to a ClearML queue.
+def sleep_countdown(t_sleep: int) -> None:
+    logger = logging.getLogger()
 
-    Usage: python enqueue_tasks.py <base_task_id> <tasks_num> <queue_name> [--tags TAG1 TAG2] [--parameters JSON_STRING]
-    """
+    # Countdown loop with logging
+    for i in range(t_sleep, -1, -1):
+        logger.info(f"{i} seconds remaining... ")
+        sys.stdout.write("\r")
+        sys.stdout.flush()
+        time.sleep(1)
 
-    parser = argparse.ArgumentParser(
-        description="Enqueue a batch of tasks to a given ClearML queue."
-    )
-    parser.add_argument(
-        "--base-task-id", help="The ID of the base task to clone for each job."
-    )
-    parser.add_argument("--tasks-num", type=int, help="The number of tasks to enqueue.")
-    parser.add_argument(
-        "--queue-name", help="The name of the ClearML queue to enqueue the tasks to."
-    )
-    parser.add_argument("--tags", nargs="*", help="Optional tags to add to each task.")
-    parser.add_argument(
-        "--parameters", help="Optional parameter overrides as a JSON string."
-    )
-    parser.add_argument(
-        "--allow-large-batch",
-        action="store_true",
-        default=False,
-        help="Disable safeguard mechanism for sending a large batch of runs.",
-    )
-
-    args = parser.parse_args()
-
-    parameters = json.loads(args.parameters) if args.parameters else {}
-
-    if not args.tasks_num < TASKS_NUM_SAFEGUARD_LIMIT:
-        print(
-            f">>> WARNING <<< \nYou are trying to enqueue {args.tasks_num} tasks. "
-            f"The safeguard limit is {TASKS_NUM_SAFEGUARD_LIMIT} tasks. "
-            "If you REALLY want to eqnue that much, please add the `--allow-large-batch` flag."
-        )
-        return
-
-    enqueue_tasks(
-        base_task_id=args.base_task_id,
-        tasks_num=args.tasks_num,
-        queue_name=args.queue_name,
-        tags=args.tags,
-        parameters=parameters,
-    )
+    # Completion message
+    logger.info("Proceeding...\n")
+    sys.stdout.flush()
 
 
 if __name__ == "__main__":
+    setup_logging()
+    log = logging.getLogger(f"{__name__}")
     main()
+    log.info("Exiting")
