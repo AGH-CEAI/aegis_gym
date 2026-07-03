cda8e123f0e5837c91a8584b9b77ccae5bfb2e44
macmacal <mac.aleksandrowicz@gmail.com>
2026-07-03 21:27:29 +0200

    Updated BaseENV api to include modalities

--- aegis_gym/rsl/envs/base_env.py
+++ aegis_gym/rsl/envs/base_env.py
@@ -1,6 +1,10 @@
 from abc import abstractmethod
-from typing import NamedTuple
+from enum import auto
+from typing import NamedTuple, Optional, Callable
+
+
 import torch as th
+from strenum import StrEnum
 from tensordict import TensorDict
 from rsl_rl.env import VecEnv
 
@@ -8,34 +12,65 @@ from .scene.base_scene import BaseScene
 
 
 class ResetReturn(NamedTuple):
+    """This return tuple is compatible with the rsl_rl's `OnPolicyRunner`"""
+
     observations: TensorDict
     extras: dict
 
 
 class StepReturn(NamedTuple):
+    """This return tuple is compatible with the rsl_rl's `OnPolicyRunner`"""
+
     observations: TensorDict
     rewards: th.Tensor
     dones: th.Tensor
     extras: dict
 
 
+class Modality(StrEnum):
+    TCP_POSE = auto()
+    TCP_VELOCITY = auto()
+    TCP_WRENCH = auto()
+    CAMERA_SCENE_RGB = auto()
+    CAMERA_TOOL_LEFT_RGB = auto()
+    CAMERA_TOOL_RIGHT_RGB = auto()
+    OBJECT_POSE = auto()
+
+
 class BaseEnv(VecEnv):
     """
     Base class for implementing an environment compatible with rsl_rl's VecEnv.
     See https://github.com/leggedrobotics/rsl_rl/blob/main/rsl_rl/env/vec_env.py
+    You need to define the `DEFAULT_MODALITIES` and `_observation_fns` manually.
     """
 
-    def __init__(self, scene: BaseScene):
+    DEFAULT_MODALITIES: frozenset[Modality]
+    _observation_fns: dict[Modality, Callable[[], th.Tensor]]
+
+    def __init__(self, scene: Optional[BaseScene]):
         super().__init__()
-        self._scene: BaseScene = scene
+        self._scene: Optional[BaseScene] = scene
+        self._obs_cache: TensorDict = TensorDict({}, batch_size=[self.get_num_envs()])
 
     def __del__(self):
         if self._scene:
             self._scene.shutdown()
 
+    def _obs_cache_get(self, modalities: frozenset[Modality]) -> TensorDict:
+        return self._obs_cache.select(*(m.value for m in modalities))
+
+    def _obs_cache_set(self, modality: Modality, value: th.Tensor) -> None:
+        self._obs_cache.set(modality.value, value)
+
+    def _obs_cache_keys(self) -> frozenset[Modality]:
+        return frozenset(Modality(k) for k in self._obs_cache.keys())
+
+    def _obs_cache_clear(self) -> None:
+        self._obs_cache = TensorDict({}, batch_size=[self.get_num_envs()])
+
     @property
     def unwrapped(self) -> "BaseEnv":
-        """Required by rsl_rl logger."""
+        """Get the underlying environment."""
         return self
 
     @property
@@ -47,6 +82,11 @@ class BaseEnv(VecEnv):
     def cfg(self) -> dict:
         """Required by rsl_rl logger."""
         return self.get_cfg_as_dict()
+
+    @property
+    def available_modalities(self) -> frozenset[Modality]:
+        """Returns set of available observation modalities"""
+        return frozenset(self._observation_fns)
 
     @abstractmethod
     def get_policy_dt(self) -> float:
@@ -63,9 +103,41 @@ class BaseEnv(VecEnv):
         """Returns the number of parallel environments (1 for real robot)."""
         ...
 
+    def get_observations(
+        self, modalities: Optional[frozenset[Modality]] = None
+    ) -> TensorDict:
+        """
+        Returns observations at the current state. Derived from rsl_rl's `VecEnv`.
+        Different `modalities` can be obtained in the result `TensorDict`.
+        """
+        modalities = self._resolve_modalities(modalities)
+        for m in modalities - self._obs_cache_keys():
+            self._obs_cache_set(m, self._observation_fns[m]())
+        return self._obs_cache_get(modalities)
+
+    def _resolve_modalities(
+        self, modalities: Optional[frozenset[Modality]]
+    ) -> frozenset[Modality]:
+        if modalities is None:
+            return self.DEFAULT_MODALITIES
+        unknown = modalities - self.available_modalities
+        if unknown:
+            raise ValueError(f"Unsupported modalities: {unknown}")
+        return modalities
+
+    def get_agent_observations(self) -> TensorDict:
+        """Adapter: produces (policy_obs, extras) in the shape rsl_rl's `OnPolicyRunner` expects."""
+
+        policy_obs = self._agent_observations_mapping()
+        # TODO: incorporate more rsl_rl group obs
+        return TensorDict({"policy": policy_obs}, batch_size=self.get_num_envs())
+
     @abstractmethod
-    def get_observations(self) -> TensorDict:
-        """Returns observations at the current state. Derived from VecEnv."""
+    def _agent_observations_mapping(self) -> th.Tensor:
+        """
+        Maps default modalities from `get_observations()` into agent observation.
+        Should be configured via given config.
+        """
         ...
 
     @abstractmethod
