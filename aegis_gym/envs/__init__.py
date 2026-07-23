0abc6d86edeed1a6561dfaf699ec6b3880845c5b
Maciej Aleksandrowicz <mac.aleksandrowicz@gmail.com>
2026-07-23 13:29:09 +0200

    Removed old aegis_gym abstractions

--- aegis_gym/envs/__init__.py
+++
@@ -1,3 +0,0 @@
-from .aegis_reacher import AegisReacherEnv  # noqa: F401
-from .aegis_pusher import AegisPusherEnv  # noqa: F401
-# from .env_types import EnvControlType, EnvObservationType, EnvRenderMode, EnvRewardType  # noqa F401
--- test/import_test.py
+++
@@ -1,6 +0,0 @@
-def test_import():
-    import aegis_gym  # noqa: F401
-
-
-def test_import_AegisReacherEnv():
-    from aegis_gym.envs import AegisReacherEnv  # noqa: F401
