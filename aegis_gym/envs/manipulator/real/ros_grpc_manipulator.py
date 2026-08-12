01ca77ee838fb5833651fea8c262bf37c138d4f0
AntoniRL <aradominski@agh.edu.pl>
2026-08-12 14:35:37 +0200

    test for checks

--- aegis_gym/envs/manipulator/real/ros_grpc_manipulator.py
+++ aegis_gym/envs/manipulator/real/ros_grpc_manipulator.py
@@ -9,8 +9,6 @@ from tensordict import TensorDict
 
 from aegis_gym.aux import get_logger
 
-logger = get_logger(__name__)
-
 try:
     from aegis_grpc_client import (
         AegisJointIndex,
@@ -21,7 +19,7 @@ try:
     )
     from aegis_grpc_client import CameraName as RosGrpcCameraName
 except ImportError:
-    logger.info(
+    print(
         "Failed to import aegis_grpc_client. "
         "Double check if you have installed the `aegis_grpc_client` and `proto_aegis_grpc` packages."
     )
