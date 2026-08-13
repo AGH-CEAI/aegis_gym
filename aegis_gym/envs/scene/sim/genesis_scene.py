8ffb3c5f98f9fa7cd80d7a4511721fbd555a9fa4
Jakub <sivral@proton.me>
2026-08-13 18:34:11 +0200

    Change friction

--- aegis_gym/envs/scene/sim/genesis_scene.py
+++ aegis_gym/envs/scene/sim/genesis_scene.py
@@ -176,7 +176,7 @@ class GenesisScene(BaseScene):
                 fixed=True,
             ),
             surface=gs.surfaces.Default(color=(1.0, 0.96, 0.92)),
-            material=gs.materials.Rigid(friction=0.6, coup_friction=0.6),
+            material=gs.materials.Rigid(friction=0.4, coup_friction=0.4),
         )
 
     def _setup_create_cameras(
