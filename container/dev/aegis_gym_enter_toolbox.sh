5a3058b6f9fc21eab1695c81b0657a5e9f41f148
AntoniRL <aradominski@agh.edu.pl>
2026-08-26 11:50:04 +0200

    change contaierbuilding

--- container/dev/aegis_gym_enter_toolbox.sh
+++ container/dev/aegis_gym_enter_toolbox.sh
@@ -17,7 +17,7 @@ cuda_short_version="cu129"
 torch_version="2.8.0"
 torchvision_version="0.23.0"
 
-aegis_gym_tag=${AEGIS_GYM_TAG:-v202606181210}
+aegis_gym_tag=${AEGIS_GYM_TAG:-devel}
 ceai_rsl_rl_tag=${CEAI_RSL_RL_TAG:-v3.3.2}
 aegis_ros_tag=${AEGIS_ROS_TAG:-humble-devel}
 
